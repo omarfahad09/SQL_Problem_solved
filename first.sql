55d2f1ab58c0469ebd1f0b076e4facecdec0b4e6
omarfahad09 <omarfahad0009@gmail.com>
2025-07-02 22:33:19 +0600

    Update first.sql

--- first.sql
+++ first.sql
@@ -1,1 +1,17 @@
+#QUESTION
+Given a table of candidates and their skills, you're tasked with finding the candidates best suited for an open Data Science job. You want to find candidates who are proficient in Python, Tableau, and PostgreSQL.
 
+Write a query to list the candidates who possess all of the required skills for the job. Sort the output by candidate ID in ascending order.
+
+# Candidate Skills Table
+
+| candidate_id | skill        |
+|--------------|--------------|
+| 123          | Python       |
+| 123          | Tableau      |
+| 123          | PostgreSQL   |
+| 234          | R            |
+| 234          | PowerBI      |
+| 234          | SQL Server   |
+| 345          | Python       |
+| 345          | Tableau      |
